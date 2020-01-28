6b578bc65263eb8be55e808a33e4c4289a2f98aa
Tonokip <tonokip@gmail.com>
2020-01-28 12:14:39 -0600

    working over usart on atsam3x8e

--- spi_flash_programmer/spi_flash_programmer.ino
+++ spi_flash_programmer/spi_flash_programmer.ino
@@ -1,7 +1,7 @@
 #include <stdint.h>
 #include <stdio.h>
 
-#include <SPI.h>
+//#include <SPI.h>
 
 // Commands always use other characters than 0-9 a-f A-F
 // since the command data is always encoded in HEX
@@ -71,12 +71,64 @@ void impl_wait_for_write_enable(void);
 uint8_t buffer [PAGE_SIZE];
 uint8_t nCsIo;
 
+#define LFE5U_SPIFLASH_CS    23 // PA14 23 
+#define PROGRAMN_PIN 94
+#define LED_BUSY_PIN 35
+
+void USART1config() {
+  USART1->US_WPMR = 0x55534100;   // Unlock the USART Mode register
+  USART1->US_MR |= 0x408CE | 0x100;       // Set Mode to CLK0=1, 8_BIT, SPI_MASTER
+  USART1->US_BRGR = 21;           // Clock Divider (SCK = 4MHz)
+
+  PIOA->PIO_WPMR = 0x50494F00;    // Unlock PIOA Write Protect Mode Register
+  PIOB->PIO_WPMR = 0x50494F00;    // Unlock PIOB Write Protect Mode Register
+  PIOA->PIO_ABSR |= (0u << 14);   // CS: Assign A14 I/O to the Peripheral A function
+  PIOA->PIO_PDR |= (1u << 14);    // CS: Disable PIO control, enable peripheral control
+  PIOA->PIO_ABSR |= (0u << 16);   // SCK: Assign A16 I/O to the Peripheral A function
+  PIOA->PIO_PDR |= (1u << 16);    // SCK: Disable PIO control, enable peripheral control
+  PIOA->PIO_ABSR |= (0u << 13);   // MOSI: Assign PA13 I/O to the Peripheral A function
+  PIOA->PIO_PDR |= (1u << 13);    // MOSI: Disable PIO control, enable peripheral control
+  PIOA->PIO_ABSR |= (0u << 12);   // MISO: Assign A12 I/O to the Peripheral A function
+  PIOA->PIO_PDR |= (1u << 12);    // MISO: Disable PIO control, enable peripheral control
+}
+
+uint32_t uspi_init()
+{
+  Serial2.begin(9600);  // pre-initialize USART1
+  USART1config();       // setup mode, clock and pins
+  //USART1status();
+
+  pinMode(LFE5U_SPIFLASH_CS,OUTPUT);
+  digitalWrite(LFE5U_SPIFLASH_CS,HIGH);
+
+  pinMode(PROGRAMN_PIN,OUTPUT);
+  digitalWrite(PROGRAMN_PIN,LOW);
+
+  pinMode(LED_BUSY_PIN,OUTPUT);
+  digitalWrite(LED_BUSY_PIN,LOW);
+}
+
+uint32_t uspi_transfer(uint32_t data)
+{
+    
+    USART1->US_THR = data;
+    //SerialUSB.print("  RX: ");
+    delayMicroseconds(4);
+    while( ! ((USART1->US_CSR  >> 1) & 0x1u) );
+    uint8_t response = USART1->US_RHR;
+    //delayMicroseconds(1);
+    //SerialUSB.print( response );
+    return response;
+}
+
 void setup()
 {
-  nCsIo = SS;
+  uspi_init();
+  
+  nCsIo = LFE5U_SPIFLASH_CS;
 
   // Use maximum speed with F_CPU / 2
-  SPISettings settingsA(F_CPU / 2, MSBFIRST, SPI_MODE0);
+  //SPISettings settingsA(F_CPU / 2, MSBFIRST, SPI_MODE0);
   uint16_t i;
 
   for (i = 0; i < PAGE_SIZE; i += 4)
@@ -87,14 +139,31 @@ void setup()
     buffer[i + 3] = 0xEF;
   }
 
-  Serial.begin(115200);
+  SerialUSB.begin(115200);
 
-  SPI.begin(); // Initialize pins
-  SPI.beginTransaction(settingsA);
+  //SPI.begin(); // Initialize pins
+  //SPI.beginTransaction(settingsA);
   pinMode(nCsIo, OUTPUT);
   digitalWrite(nCsIo, HIGH); // disable flash device
 
   delay(10);
+}
+
+void loop2()
+{
+  if(SerialUSB.available())
+  {
+    uint8_t cmd = SerialUSB.read();
+    switch(cmd)
+    {
+      case '?':
+        SerialUSB.println("hello");
+        //write_hex_u8(0xFF);
+        //impl_jedec_id_read();
+        spiflash_init();
+        break;
+    }
+  }
 }
 
 void loop()
@@ -104,104 +173,106 @@ void loop()
   uint16_t tmp16;
 
   // Wait for command
-  while(Serial.available() == 0) {
-    ; // Do nothing
-  }
+  //while(SerialUSB.available() == 0) {
+  //  ; // Do nothing
+  //}
 
-  int cmd = Serial.read();
+  if( ! SerialUSB.available() ) return;
+
+  int cmd = SerialUSB.read();
   switch(cmd) {
   case COMMAND_HELLO:
-    Serial.print(COMMAND_HELLO); // Echo OK
-    Serial.println(VERSION);
-    Serial.print(COMMAND_HELLO); // Echo 2nd OK
+    SerialUSB.print(COMMAND_HELLO); // Echo OK
+    SerialUSB.println(VERSION);
+    SerialUSB.print(COMMAND_HELLO); // Echo 2nd OK
     break;
 
   case COMMAND_FLASH_ERASE_ALL:
     erase_all();
-    Serial.print(COMMAND_FLASH_ERASE_ALL); // Echo OK
+    SerialUSB.print(COMMAND_FLASH_ERASE_ALL); // Echo OK
     break;
 
   case COMMAND_FLASH_ERASE_SECTOR:
     if (!read_hex_u32(&address)) {
-      Serial.print(COMMAND_ERROR); // Echo Error
+      SerialUSB.print(COMMAND_ERROR); // Echo Error
       break;
     }
 
     erase_sector(address);
-    Serial.print(COMMAND_FLASH_ERASE_SECTOR); // Echo OK
+    SerialUSB.print(COMMAND_FLASH_ERASE_SECTOR); // Echo OK
     break;
 
   case COMMAND_FLASH_READ:
     if (!read_hex_u32(&address)) {
-      Serial.print(COMMAND_ERROR); // Echo Error
+      SerialUSB.print(COMMAND_ERROR); // Echo Error
       break;
     }
 
     read_page(address);
-    Serial.print(COMMAND_FLASH_READ); // Echo OK
+    SerialUSB.print(COMMAND_FLASH_READ); // Echo OK
     dump_buffer_crc();
     break;
 
   case COMMAND_FLASH_WRITE:
     if (!read_hex_u32(&address)) {
-      Serial.print(COMMAND_ERROR); // Echo Error
+      SerialUSB.print(COMMAND_ERROR); // Echo Error
       break;
     }
 
     write_page(address);
-    Serial.print(COMMAND_FLASH_WRITE); // Echo OK
+    SerialUSB.print(COMMAND_FLASH_WRITE); // Echo OK
     break;
 
   case COMMAND_BUFFER_LOAD:
-    Serial.print(COMMAND_BUFFER_LOAD); // Echo OK
+    SerialUSB.print(COMMAND_BUFFER_LOAD); // Echo OK
     dump_buffer();
-    Serial.println();
+    SerialUSB.println();
     break;
 
   case COMMAND_BUFFER_CRC:
-    Serial.print(COMMAND_BUFFER_CRC); // Echo OK
+    SerialUSB.print(COMMAND_BUFFER_CRC); // Echo OK
     dump_buffer_crc();
-    Serial.println();
+    SerialUSB.println();
     break;
 
   case COMMAND_BUFFER_STORE:
     if (!read_into_buffer()) {
-      Serial.print(COMMAND_ERROR); // Echo Error
+      SerialUSB.print(COMMAND_ERROR); // Echo Error
       break;
     }
 
-    Serial.print(COMMAND_BUFFER_STORE); // Echo OK
+    SerialUSB.print(COMMAND_BUFFER_STORE); // Echo OK
     dump_buffer_crc();
     break;
 
   case COMMAND_WRITE_PROTECTION_CHECK:
-    Serial.print(COMMAND_WRITE_PROTECTION_CHECK); // Echo OK
+    SerialUSB.print(COMMAND_WRITE_PROTECTION_CHECK); // Echo OK
     impl_write_protection_check();
     break;
 
   case COMMAND_WRITE_PROTECTION_ENABLE:
-    Serial.print(COMMAND_WRITE_PROTECTION_ENABLE); // Echo OK
+    SerialUSB.print(COMMAND_WRITE_PROTECTION_ENABLE); // Echo OK
     impl_write_protection_enable();
     break;
 
   case COMMAND_WRITE_PROTECTION_DISABLE:
-    Serial.print(COMMAND_WRITE_PROTECTION_DISABLE); // Echo OK
+    SerialUSB.print(COMMAND_WRITE_PROTECTION_DISABLE); // Echo OK
     impl_write_protection_disable();
     break;
 
   case COMMAND_STATUS_REGISTER_READ:
-    Serial.print(COMMAND_STATUS_REGISTER_READ); // Echo OK
+    SerialUSB.print(COMMAND_STATUS_REGISTER_READ); // Echo OK
     impl_status_register_read();
     break;
 
   case COMMAND_ID_REGISTER_READ:
-    Serial.print(COMMAND_ID_REGISTER_READ); // Echo OK
+    SerialUSB.print(COMMAND_ID_REGISTER_READ); // Echo OK
     impl_jedec_id_read();
     break;
 
   case COMMAND_SET_CS:
     if(!read_hex_u8(&tmp8)) {
-      Serial.print(COMMAND_ERROR); // Echo Error
+      SerialUSB.print(COMMAND_ERROR); // Echo Error
       break;
     }
     if (tmp8 != nCsIo) {
@@ -212,12 +283,12 @@ void loop()
       digitalWrite(nCsIo, HIGH); // disable flash device
     }
 
-    Serial.print(COMMAND_SET_CS); // Echo OK
+    SerialUSB.print(COMMAND_SET_CS); // Echo OK
     break;
 
   case COMMAND_SET_OUTPUT:
     if(!read_hex_u16(&tmp16)) {
-      Serial.print(COMMAND_ERROR); // Echo Error
+      SerialUSB.print(COMMAND_ERROR); // Echo Error
       break;
     }
     pinMode(tmp16>>8, OUTPUT);
@@ -230,37 +301,37 @@ void loop()
       }
     }
 
-    Serial.print(COMMAND_SET_OUTPUT); // Echo OK
+    SerialUSB.print(COMMAND_SET_OUTPUT); // Echo OK
     break;
 
   case COMMAND_HELP:
-    Serial.println(VERSION);
-    Serial.println("  n         : erase chip");
-    Serial.println("  kXXXXXXXX : erase 4k sector XXXXXXXX (hex)");
-    Serial.println();
-    Serial.println("  rXXXXXXXX : read a page XXXXXXXX (hex) to buffer");
-    Serial.println("  wXXXXXXXX : write buffer to a page XXXXXXXX (hex)");
-    Serial.println();
-    Serial.println("  p         : enable write protection");
-    Serial.println("  u         : disable write protection");
-    Serial.println("  x         : check write protection");
-    Serial.println("  y         : read status register");
-    Serial.println("  i         : read id register");
-    Serial.println();
-    Serial.println("  h         : print buffer CRC-32");
-    Serial.println("  l         : display the buffer (in hex)");
-    Serial.println("  sBBBBBBBB : load the buffer with a page size of data BBBBBBBB...");
-    Serial.println();
-    Serial.println("  *XX       : set IO XX as CS/SS");
-    Serial.println("  oXXYZ     : set IO XX as output, set value Z if Y!=0");
-    Serial.println();
-    Serial.println("Examples:");
-    Serial.println("  r00003700      read data from page 0x3700 into buffer");
-    Serial.println("  scafe...3737   load the buffer with a page of data, first byte is 0xca ...");
+    SerialUSB.println(VERSION);
+    SerialUSB.println("  n         : erase chip");
+    SerialUSB.println("  kXXXXXXXX : erase 4k sector XXXXXXXX (hex)");
+    SerialUSB.println();
+    SerialUSB.println("  rXXXXXXXX : read a page XXXXXXXX (hex) to buffer");
+    SerialUSB.println("  wXXXXXXXX : write buffer to a page XXXXXXXX (hex)");
+    SerialUSB.println();
+    SerialUSB.println("  p         : enable write protection");
+    SerialUSB.println("  u         : disable write protection");
+    SerialUSB.println("  x         : check write protection");
+    SerialUSB.println("  y         : read status register");
+    SerialUSB.println("  i         : read id register");
+    SerialUSB.println();
+    SerialUSB.println("  h         : print buffer CRC-32");
+    SerialUSB.println("  l         : display the buffer (in hex)");
+    SerialUSB.println("  sBBBBBBBB : load the buffer with a page size of data BBBBBBBB...");
+    SerialUSB.println();
+    SerialUSB.println("  *XX       : set IO XX as CS/SS");
+    SerialUSB.println("  oXXYZ     : set IO XX as output, set value Z if Y!=0");
+    SerialUSB.println();
+    SerialUSB.println("Examples:");
+    SerialUSB.println("  r00003700      read data from page 0x3700 into buffer");
+    SerialUSB.println("  scafe...3737   load the buffer with a page of data, first byte is 0xca ...");
     break;
   }
 
-  Serial.flush();
+  SerialUSB.flush();
 } 
 
 void read_page(uint32_t address)
@@ -354,7 +425,7 @@ int8_t read_nibble(void)
   int16_t c;
 
   do {
-    c = Serial.read();
+    c = SerialUSB.read();
   } while(c == -1);
 
   if (c >= '0' && c <= '9') {
@@ -431,9 +502,9 @@ int8_t read_hex_u32(uint32_t *value)
 void write_nibble(uint8_t value)
 {
   if (value < 10) {
-    Serial.write(value + '0' - 0);
+    SerialUSB.write(value + '0' - 0);
   } else {
-    Serial.write(value + 'A' - 10);
+    SerialUSB.write(value + 'A' - 10);
   }
 }
 
@@ -520,34 +591,35 @@ uint32_t crc_buffer(void)
 
 void impl_enable_write(void)
 {
-  SPI.transfer(WREN); // write enable
+  uspi_transfer(WREN); // write enable
 }
 
 void impl_erase_chip(void)
 {
-  SPI.transfer(CHIP_ERASE);
+  uspi_transfer(CHIP_ERASE);
 }
 
 void impl_erase_sector(uint32_t address)
 {
-  SPI.transfer(SECTOR_ERASE);            // sector erase instruction
-  SPI.transfer((address & 0x0FF0) >> 4); // bits 23 to 16
-  SPI.transfer((address & 0x000F) << 4); // bits 15 to 8
-  SPI.transfer(0);                       // bits 7 to 0
+  uspi_transfer(SECTOR_ERASE);            // sector erase instruction
+  uspi_transfer((address & 0x0FF0) >> 4); // bits 23 to 16
+  uspi_transfer((address & 0x000F) << 4); // bits 15 to 8
+  uspi_transfer(0);                       // bits 7 to 0
 }
 
 void impl_read_page(uint32_t address)
 {
   uint16_t counter;
 
-  SPI.transfer(READ);                  // read instruction
-  SPI.transfer((address >> 8) & 0xFF); // bits 23 to 16
-  SPI.transfer(address & 0xFF);        // bits 15 to 8
-  SPI.transfer(0);                     // bits 7 to 0
+  uspi_transfer(READ);                  // read instruction
+  //uspi_transfer((address >> 16) & 0xFF); // bits 23 to 16
+  uspi_transfer((address >> 8) & 0xFF); // bits 23 to 16
+  uspi_transfer(address & 0xFF);        // bits 15 to 8
+  uspi_transfer(0);                     // bits 7 to 0
 
   // Transfer a dummy page to read data
   for(counter = 0; counter < PAGE_SIZE; counter++) {
-    buffer[counter] = SPI.transfer(0xff);
+    buffer[counter] = uspi_transfer(0xff);
   }
 }
 
@@ -555,13 +627,14 @@ void impl_write_page(uint32_t address)
 {
   uint16_t counter;
 
-  SPI.transfer(WRITE);                 // write instruction
-  SPI.transfer((address >> 8) & 0xFF); // bits 23 to 16
-  SPI.transfer(address & 0xFF);        // bits 15 to 8
-  SPI.transfer(0);                     // bits 7 to 0
+  uspi_transfer(WRITE);                 // write instruction
+  //uspi_transfer((address >> 16) & 0xFF); // bits 23 to 16
+  uspi_transfer((address >> 8) & 0xFF); // bits 23 to 16
+  uspi_transfer(address & 0xFF);        // bits 15 to 8
+  uspi_transfer(0);                     // bits 7 to 0
 
   for (counter = 0; counter < PAGE_SIZE; counter++) {
-    SPI.transfer(buffer[counter]);
+    uspi_transfer(buffer[counter]);
   }
 }
 
@@ -572,8 +645,8 @@ void impl_wait_for_write_enable(void)
   while((statreg & 0x1) == 0x1) {
     // Wait for the chip
     digitalWrite(nCsIo, LOW);
-    SPI.transfer(RDSR);
-    statreg = SPI.transfer(RDSR);
+    uspi_transfer(RDSR);
+    statreg = uspi_transfer(RDSR);
     digitalWrite(nCsIo, HIGH);
   }
 }
@@ -584,20 +657,20 @@ void impl_write_protection_check(void)
 
   // Read status register 1
   digitalWrite(nCsIo, LOW);
-  SPI.transfer(RDSR);
-  statusRegister = ((uint32_t) SPI.transfer(RDSR));
+  uspi_transfer(RDSR);
+  statusRegister = ((uint32_t) uspi_transfer(RDSR));
   digitalWrite(nCsIo, HIGH);
 
   // Read status register 2
   digitalWrite(nCsIo, LOW);
-  SPI.transfer(RDSR2);
-  statusRegister |= ((uint32_t) SPI.transfer(RDSR2)) << 8;
+  uspi_transfer(RDSR2);
+  statusRegister |= ((uint32_t) uspi_transfer(RDSR2)) << 8;
   digitalWrite(nCsIo, HIGH);
 
   // Read status register 3
   digitalWrite(nCsIo, LOW);
-  SPI.transfer(RDSR3);
-  statusRegister |= ((uint32_t) SPI.transfer(RDSR3)) << 16;
+  uspi_transfer(RDSR3);
+  statusRegister |= ((uint32_t) uspi_transfer(RDSR3)) << 16;
   digitalWrite(nCsIo, HIGH);
 
   if (statusRegister & SRP1) {
@@ -640,36 +713,36 @@ void impl_write_protection_disable(void)
 
   // Read status register 1
   digitalWrite(nCsIo, LOW);
-  SPI.transfer(RDSR);
-  statusRegister = SPI.transfer(RDSR);
+  uspi_transfer(RDSR);
+  statusRegister = uspi_transfer(RDSR);
   digitalWrite(nCsIo, HIGH);
 
   // Read status register 2
   digitalWrite(nCsIo, LOW);
-  SPI.transfer(RDSR2);
-  statusRegister2 = SPI.transfer(RDSR2);
+  uspi_transfer(RDSR2);
+  statusRegister2 = uspi_transfer(RDSR2);
   digitalWrite(nCsIo, HIGH);
 
   // Set chip as writable
   digitalWrite(nCsIo, LOW);
-  SPI.transfer(WREN); // Write enable
+  uspi_transfer(WREN); // Write enable
   digitalWrite(nCsIo, HIGH);
   delay(10);
 
   digitalWrite(nCsIo, LOW);
-  SPI.transfer(WRSR);                         // Write register instruction
-  SPI.transfer(statusRegister & ~BP);         // Force SR1 to XXX000XX
+  uspi_transfer(WRSR);                         // Write register instruction
+  uspi_transfer(statusRegister & ~BP);         // Force SR1 to XXX000XX
   digitalWrite(nCsIo, HIGH);
 
   // Set chip as writable
   digitalWrite(nCsIo, LOW);
-  SPI.transfer(WREN); // Write enable
+  uspi_transfer(WREN); // Write enable
   digitalWrite(nCsIo, HIGH);
   delay(10);
 
   digitalWrite(nCsIo, LOW);
-  SPI.transfer(WRSR2);                        // Write register 2 instruction
-  SPI.transfer(statusRegister2 & ~(CP >> 8)); // Force SR2 to X0XXXXXX
+  uspi_transfer(WRSR2);                        // Write register 2 instruction
+  uspi_transfer(statusRegister2 & ~(CP >> 8)); // Force SR2 to X0XXXXXX
   digitalWrite(nCsIo, HIGH);
   delay(1);
 
@@ -683,36 +756,36 @@ void impl_write_protection_enable(void)
 
   // Read status register 1
   digitalWrite(nCsIo, LOW);
-  SPI.transfer(RDSR);
-  statusRegister = SPI.transfer(RDSR);
+  uspi_transfer(RDSR);
+  statusRegister = uspi_transfer(RDSR);
   digitalWrite(nCsIo, HIGH);
 
   // Read status register 2
   digitalWrite(nCsIo, LOW);
-  SPI.transfer(RDSR2);
-  statusRegister2 = SPI.transfer(RDSR2);
+  uspi_transfer(RDSR2);
+  statusRegister2 = uspi_transfer(RDSR2);
   digitalWrite(nCsIo, HIGH);
 
   // Set chip as writable
   digitalWrite(nCsIo, LOW);
-  SPI.transfer(WREN); // Write enable
+  uspi_transfer(WREN); // Write enable
   digitalWrite(nCsIo, HIGH);
   delay(10);
 
   digitalWrite(nCsIo, LOW);
-  SPI.transfer(WRSR);                         // Write register instruction
-  SPI.transfer(statusRegister | BP);          // Force SR1 to XXX111XX
+  uspi_transfer(WRSR);                         // Write register instruction
+  uspi_transfer(statusRegister | BP);          // Force SR1 to XXX111XX
   digitalWrite(nCsIo, HIGH);
 
   // Set chip as writable
   digitalWrite(nCsIo, LOW);
-  SPI.transfer(WREN); // Write enable
+  uspi_transfer(WREN); // Write enable
   digitalWrite(nCsIo, HIGH);
   delay(10);
 
   digitalWrite(nCsIo, LOW);
-  SPI.transfer(WRSR2);                        // Write register 2 instruction
-  SPI.transfer(statusRegister2 & ~(CP >> 8)); // Force SR2 to X0XXXXXX
+  uspi_transfer(WRSR2);                        // Write register 2 instruction
+  uspi_transfer(statusRegister2 & ~(CP >> 8)); // Force SR2 to X0XXXXXX
   digitalWrite(nCsIo, HIGH);
   delay(1);
 
@@ -727,20 +800,20 @@ void impl_status_register_read(void)
 
   // Read status register 1
   digitalWrite(nCsIo, LOW);
-  SPI.transfer(RDSR);
-  statusRegister = SPI.transfer(RDSR);
+  uspi_transfer(RDSR);
+  statusRegister = uspi_transfer(RDSR);
   digitalWrite(nCsIo, HIGH);
 
   // Read status register 2
   digitalWrite(nCsIo, LOW);
-  SPI.transfer(RDSR2);
-  statusRegister2 = SPI.transfer(RDSR2);
+  uspi_transfer(RDSR2);
+  statusRegister2 = uspi_transfer(RDSR2);
   digitalWrite(nCsIo, HIGH);
 
   // Read status register 3
   digitalWrite(nCsIo, LOW);
-  SPI.transfer(RDSR3);
-  statusRegister3 = SPI.transfer(RDSR3);
+  uspi_transfer(RDSR3);
+  statusRegister3 = uspi_transfer(RDSR3);
   digitalWrite(nCsIo, HIGH);
 
   // Send status register length
@@ -755,11 +828,30 @@ void impl_status_register_read(void)
 void impl_jedec_id_read(void)
 {
   digitalWrite(nCsIo, LOW);
-  SPI.transfer(JEDECIDR);
+  delayMicroseconds(1);
+  uspi_transfer(JEDECIDR);
   write_hex_u8(0x03);
-  write_hex_u8(SPI.transfer(0x0));
-  write_hex_u8(SPI.transfer(0x0));
-  write_hex_u8(SPI.transfer(0x0));
+  write_hex_u8(uspi_transfer(0x0));
+  write_hex_u8(uspi_transfer(0x0));
+  write_hex_u8(uspi_transfer(0x0));
   digitalWrite(nCsIo, HIGH);
 }
 
+void spiflash_init()
+{
+  //pinMode(SPIFLASH_CS,OUTPUT);
+  //digitalWrite(SPIFLASH_CS,HIGH);
+  //SPI.begin(SPIFLASH_CS);
+  digitalWrite(nCsIo,LOW);
+  // MFG ID
+  SerialUSB.print(PSTR("SPIFLASH_MFG_ID: ")); 
+  SerialUSB.print( uspi_transfer(0x9F) );
+  SerialUSB.print(" "); 
+  SerialUSB.print( uspi_transfer(0x00) );
+  SerialUSB.print(" "); 
+  SerialUSB.print( uspi_transfer(0x00) );
+  SerialUSB.print(" "); 
+  SerialUSB.println( uspi_transfer(0x00) );
+
+  digitalWrite(nCsIo,HIGH);
+}
